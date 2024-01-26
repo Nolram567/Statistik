95d6dc3dba3530a57e70aee350902c9e34e573a9
Nolram567 <74004417+Nolram567@users.noreply.github.com>
2024-01-26 12:27:18 +0100

    Add files via upload

---
+++ Assignment_5.R
@@ -0,0 +1,167 @@
+relativer_dateipfad <- "data/Daten_Fragebogen.csv"
+
+daten <- read.csv(relativer_dateipfad, sep= ";")
+
+head(daten)
+
+#a)
+class(daten)
+
+v <- daten$Km
+
+mean(v)
+median(v)
+sd(v)
+var(v)
+
+#b)
+
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+
+v2 <- daten$Alter
+
+mean(v2)
+sd(v2)
+
+getmode(v2)
+
+#c)
+mean(daten$Alter[daten$Geschlecht == "m"])
+
+mean(daten$Alter[daten$Geschlecht == "f"])
+
+#d) - f)
+
+hist(v2)
+hist(daten$Alter[daten$Geschlecht == "m"])
+hist(daten$Alter[daten$Geschlecht == "f"], xlab = "Alter der Frauen")
+
+pdf("HistogrammAlter.pdf")
+hist(v2, col= "red")
+dev.off()
+
+#Aufgabe 2: Fibonacci-Funktion
+
+fib <- function(n){
+  
+  if(n == 1){return (1)}
+  if(n == 2){return (1)}
+  
+  return (fib(n-1) + fib(n-2))
+  
+}
+
+save(fib, file = "fibonacci_function.R")
+
+load("fibonacci_function.R")
+
+fib(6)
+fib(9)
+
+#Aufgabe 4
+
+
+bd <- read.csv("Blutdruck.csv", sep = ";")
+mean(bd$Blutdruck)
+sd(bd$Blutdruck)
+
+hist(bd$Blutdruck, xlab = "Blutdruck")
+
+# Zweiseitiger t-test: Signifikanzlevel α = 0.05; Konfidenzlevel = 1-α;Angenommener Mittelwert μ = 17
+# H0 : Der mittlere Blutdruck weicht nach der Gabe des Medikaments nicht signifikant von Mittelwert 17 ab.
+# H1 : Der mittlere Blutdruck ist nach der Gabe des Medikaments signifikant kleiner oder größer als 17.
+
+t.test(bd$Blutdruck, alternative = "two.sided", mu = 17, conf.level = 0.99)
+
+#data:  bd$Blutdruck
+#t = -2.2247, df = 99, p-value = 0.02837
+#alternative hypothesis: true mean is not equal to 17
+#95 percent confidence interval:
+#  16.19972 16.95428
+#sample estimates:
+#  mean of x 
+#16.577 
+# H0 wird verworfen, da p < α
+
+
+# Zweiseitiger t-test: Signifikanzlevel α = 0.01; Konfidenzlevel = 1-α;Angenommener Mittelwert μ = 17
+t.test(bd$Blutdruck, alternative = "two.sided", mu = 17, conf.level = 0.99)
+
+# data:  bd$Blutdruck
+# t = -2.2247, df = 99, p-value = 0.02837
+# alternative hypothesis: true mean is not equal to 17
+# 99 percent confidence interval:
+#  16.07762 17.07638
+#sample estimates:
+#  mean of x 
+# 16.577
+# H0 wird akzeptiert, da p > α
+
+
+# Einseitiger t-test: Signifikanzlevel α = 0.05; Konfidenzlevel = 1-α;Angenommener Mittelwert μ = 17
+# H0: Der mittlere Blutdruck weicht nach der Gabe des Medikaments nicht kleiner als 17.
+# H1 : Der mittlere Blutdruck ist nach der Gabe des Medikaments kleiner als 17
+
+t.test(bd$Blutdruck, alternative = "less", mu = 17, conf.level = 0.95)
+
+#data:  bd$Blutdruck
+#t = -2.2247, df = 99, p-value = 0.01419
+#alternative hypothesis: true mean is less than 17
+#95 percent confidence interval:
+#  -Inf 16.89271
+#sample estimates:
+#  mean of x 
+#16.577
+# H0 wird verworfen, da p < α
+
+# Einseitiger t-test: Signifikanzlevel α = 0.05; Konfidenzlevel = 1-α;Angenommener Mittelwert μ = 17
+# H0: Der mittlere Blutdruck ist nach der Gabe des Medikaments größer als in der Kontrollgruppe.
+# H1 : Der mittlere Blutdruck ist nach der Gabe des Medikaments größer als in der Kontrollgruppe.
+t.test(bd$Blutdruck, alternative = c("greater"), mu = 17, conf.level = 0.95)
+
+#data:  bd$Blutdruck
+#t = -2.2247, df = 99, p-value = 0.9858
+#alternative hypothesis: true mean is greater than 17
+#95 percent confidence interval:
+#  16.26129      Inf
+#sample estimates:
+#  mean of x 
+#16.577 
+# H0 wird angenommen, da p > α
+
+# Aufgabe 5
+
+data <- read.csv("effect.csv", sep = ";")
+
+attach(data)
+Diff[Group == "A"]
+# Das Resultat sind alle Werte in der Spalte "Diff", in deren Zeile unter "Group" "A" steht.
+
+A <- Diff[Group == "A"]
+B <- Diff[Group == "B"]
+
+cat(B)
+
+mean_A <- mean(A)
+mean_B <- mean(B) 
+
+
+# Zweiseitiger t-test: Signifikanzlevel α = 0.05; Konfidenzintervall = 1-α; Angenommener Mittelwert μ = mean(A)
+# H0 : Die Mittelwerte von A und B sind gleich
+# H1 : Die Mittelwerte von A und B sind nicht gleich
+t.test(A, B, alternative = "two.sided", conf.level = 0.95)
+
+#Welch Two Sample t-test
+#data:  A and B
+#t = -0.96989, df = 163.62, p-value = 0.3335
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -0.6952132  0.2372132
+#sample estimates:
+#  mean of x mean of y 
+#0.467     0.696 
+# H0 wird angenommen, da p > α
